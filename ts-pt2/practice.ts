63f53bf036b76460dbf9dce1c465b2a7ee67bd2d
Howakyl96 <howakyl@gmail.com>
2021-02-05 16:35:39 -0800

    throw new Error syntax

--- ts-pt2/practice.ts
+++ ts-pt2/practice.ts
@@ -1,6 +1,9 @@
 console.log('hello, world')
 
 function add(n1: number, n2: number) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect Input!')
+    }
     return n1 + n2;
 }
 
