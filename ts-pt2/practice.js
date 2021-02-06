77f568e6425eec93ee6b53178e51f888d7ab403f
Howakyl96 <howakyl@gmail.com>
2021-02-05 16:44:57 -0800

    typing with booleans and strings

--- ts-pt2/practice.js
+++ ts-pt2/practice.js
@@ -1,11 +1,18 @@
 console.log('hello, world');
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect Input!');
+function add(n1, n2, showResult, resultPhrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect Input!')
+    // }
+    if (showResult) {
+        console.log(resultPhrase + n1 + n2);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
+// console.log(result);
