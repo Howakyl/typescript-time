3f07a28834163fdb2be7a9612687213d876330c0
Howakyl96 <howakyl@gmail.com>
2021-02-06 10:52:30 -0800

    array types

--- ts-pt2/basics.js
+++ ts-pt2/basics.js
@@ -1,5 +1,13 @@
 var person = {
     name: 'Kyle',
-    age: 24
+    age: 24,
+    hobbies: ['gaming', 'cooking']
 };
+var activites;
+// any[] type is a catch-all. Don't use it too often! Otherwise, what's the point of using TS?
+activites = ['sleeping'];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- ts-pt2/basics.ts
+++ ts-pt2/basics.ts
@@ -1,9 +1,16 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+const person = {
     name: 'Kyle',
-    age: 24
+    age: 24,
+    hobbies: ['gaming' , 'cooking']
 };
 
-console.log(person)
+let activites: string[];
+// any[] type is a catch-all. Don't use it too often! Otherwise, what's the point of using TS?
+activites = ['sleeping'];
+
+console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) This would error! This is a great example of why to use TS
+}
