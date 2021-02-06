7403c28737ccf38a02533efe701cd68a457c2c1a
Howakyl96 <howakyl@gmail.com>
2021-02-06 10:44:32 -0800

    linked index to basics.js , ts explicit object types

---
+++ ts-pt2/basics.js
@@ -0,0 +1,5 @@
+var person = {
+    name: 'Kyle',
+    age: 24
+};
+console.log(person);
---
+++ ts-pt2/basics.ts
@@ -0,0 +1,9 @@
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Kyle',
+    age: 24
+};
+
+console.log(person)
